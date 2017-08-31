c8b2656a37f726156dfad33abc1d236e528cce7e
Nathan <nathanbanstess@gmail.com>
2017-08-31 13:15:38 +1000

    Removed the redundant ordering of the processes. Documented.

--- PriorityRoundRobin.java
+++ PriorityRoundRobin.java
@@ -1,57 +1,27 @@
+/*
+  PriorityRoundRobin.java
+
+  Author: Nathan Anstess
+  Student Number: c3202195
+  Date created: 24/08/17
+  Last updated: 29/05/17
+
+	Description: 	An implementation of a Scheduling Algorithm which represents a Round Robin algorithm
+								where the time quantum is determined by the priority of the process.
+*/
+
 import java.util.ArrayList;
 
 public class PriorityRoundRobin implements SchedulingAlgorithm {
 
-	private Time time;
-
-	PriorityRoundRobin() {
-		time = Time.getInstance();
-	}	
+	PriorityRoundRobin() {}	
 
 	public String getAlgorithmName() {
 		return "PRR";
 	}
 
 	public void orderProcesses(ArrayList<Process> processes) {
-		if (processes == null || processes.size() == 0){
-				return;
-		}
-
-		int minIndex = -1;
-		int minPriority = -1;
-		boolean found = false;
-
-		for(int i = processes.size()-1; i >= 0; i--) {
-			found = false;
-			minIndex = -1;
-			minPriority = -1;
-			for(int j = 0; j <= i; j++) {
-				if(processes.get(j).getQueueEntryID() > minPriority) {	
-					minPriority = processes.get(j).getQueueEntryID();
-					minIndex = j;
-					found = true;
-				}
-				else if(processes.get(j).getQueueEntryID() == minPriority) {
-					if(processes.get(j).compareID(processes.get(minIndex)) >= 0) {
-						minPriority = processes.get(j).getQueueEntryID();
-						minIndex = j;
-						found = true;
-					}
-				}
-			}
-			if(found) {
-				swap(minIndex, i, processes);
-			}
-		}
-	}
-
-	public void swap(int index1, int index2, ArrayList<Process> processes) {
-		if(index1 == index2) {
-			return;
-		}
-		Process temp = processes.get(index1);
-		processes.set(index1, processes.get(index2));
-		processes.set(index2, temp);
+		// No ordering needed, as it is a first come first served algorithm with time quantums.
 	}
 
 	public boolean preemptive() {
