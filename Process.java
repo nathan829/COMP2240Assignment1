dcc82fc898e90c9a2b3da097edca0f9e9c7cd022
Nathan <nathanbanstess@gmail.com>
2017-08-31 13:16:31 +1000

    Completed boolean moved from the processor class to this process class. Documented.

--- Process.java
+++ Process.java
@@ -1,120 +1,151 @@
+/*
+  Process.java
+
+  Author: Nathan Anstess
+  Student Number: c3202195
+  Date created: 24/08/17
+  Last updated: 29/05/17
+
+  Description: 	A Process object which holds history data about where it has been
+                and the times at which these events occured.
+*/
+
 import java.util.ArrayList;
 
 public class Process {
 
-	private final String ID;
-	private int priority;
-	private int serviceTime;
-	private int arrivalTime;
-	private int timeEnteredReadyQueue;
-	private int timeCompleted;
+  private final String ID;
 
-	private ArrayList<Integer> timesEnteredCPU;
-	private ArrayList<Integer> timesLeftCPU;
-	private int totalTimeInCPU;
+  private int priority;
+  private int serviceTime;
+  private int arrivalTime;
+  
+  private int timeEnteredReadyQueue;
+  private int timeCompleted;
 
-	private int queueEntryID;
+  // Processor time events.
+  private ArrayList<Integer> timesEnteredCPU;
+  private ArrayList<Integer> timesLeftCPU;
+  private int totalTimeInCPU;
 
-	Process(String id) {
-		ID = id;
-		serviceTime = 0;
-		arrivalTime = 0;
-		priority = 5;       // Lowest priority as default.
-		totalTimeInCPU = 0;
-		timesEnteredCPU = new ArrayList<>();
-		timesLeftCPU = new ArrayList<>();
-		timeCompleted = -1;
-		queueEntryID = -1;
-	}
+  // A unique numerical ID given on entry to ready queue. Serves as
+  // a way of determining which process entered the queue first.
+  private int queueEntryID;
 
-	public void setQueueEntryID(int id) {
-		queueEntryID = id;
-	} 
+  Process(String id) {
+    ID = id;
+    serviceTime = 0;
+    arrivalTime = 0;
+    priority = 5;       // Lowest priority as default.
 
-	public int getQueueEntryID() {
-		return queueEntryID;
-	}
+    totalTimeInCPU = 0;
+    timesEnteredCPU = new ArrayList<>();
+    timesLeftCPU = new ArrayList<>();
+    timeCompleted = -1;
+    queueEntryID = -1;
+  }
 
-	public void setTimeEnteredCPU(int timeEnteredCPU) {
-		timesEnteredCPU.add(timeEnteredCPU);
-	}
+  public void setQueueEntryID(int id) {
+    queueEntryID = id;
+  } 
 
-	public int getTimeLastEnteredCPU() {
-		return timesEnteredCPU.get(timesEnteredCPU.size()-1);
-	}
+  public int getQueueEntryID() {
+    return queueEntryID;
+  }
 
-	public ArrayList<Integer> timesEnteredCPU() {
-		return timesEnteredCPU;
-	}
+  public void setTimeEnteredCPU(int timeEnteredCPU) {
+    timesEnteredCPU.add(timeEnteredCPU);
+  }
 
-	public void setTimeLeftCPU(int timeLeftCPU) {
-		timesLeftCPU.add(timeLeftCPU);
-	}
+  public int getTimeLastEnteredCPU() {
+    return timesEnteredCPU.get(timesEnteredCPU.size()-1);
+  }
 
-	public ArrayList<Integer> timesLeftCPU() {
-		return timesLeftCPU;
-	}
+  public ArrayList<Integer> timesEnteredCPU() {
+    return timesEnteredCPU;
+  }
 
-	public void setTimeEnteredReadyQueue(int time) {
-		timeEnteredReadyQueue = time;
-	}
+  public void setTimeLeftCPU(int timeLeftCPU) {
+    timesLeftCPU.add(timeLeftCPU);
+  }
 
-	public int getTimeEnteredReadyQueue() {
-		return timeEnteredReadyQueue;
-	}
+  public ArrayList<Integer> timesLeftCPU() {
+    return timesLeftCPU;
+  }
 
-	public void setPriority(int priority) {
-		this.priority = priority;
-	}
+  public void setTimeEnteredReadyQueue(int time) {
+    timeEnteredReadyQueue = time;
+  }
 
-	public int getPriority() {
-		return priority;
-	}
+  public int getTimeEnteredReadyQueue() {
+    return timeEnteredReadyQueue;
+  }
 
-	public void setArrivalTime(int arrivalTime) {
-		this.arrivalTime = arrivalTime;
-	}
+  public void setPriority(int priority) {
+    this.priority = priority;
+  }
 
-	public int getArrivalTime() {
-		return arrivalTime;
-	}
+  public int getPriority() {
+    return priority;
+  }
 
-	public void setTimeCompleted(int timeCompleted) {
-		this.timeCompleted = timeCompleted;
-	}
+  public void setArrivalTime(int arrivalTime) {
+    this.arrivalTime = arrivalTime;
+  }
 
-	public int getTimeCompleted() {
-		return timeCompleted;
-	}
+  public int getArrivalTime() {
+    return arrivalTime;
+  }
 
-	public void setServiceTime(int serviceTime) {
-		this.serviceTime = serviceTime;
-	}
+  public void setTimeCompleted(int timeCompleted) {
+    this.timeCompleted = timeCompleted;
+  }
 
-	public int getServiceTime() {
-		return serviceTime;
-	}
+  public int getTimeCompleted() {
+    return timeCompleted;
+  }
 
-	public String getID() {
-		return ID;
-	}
+  public void setServiceTime(int serviceTime) {
+    this.serviceTime = serviceTime;
+  }
 
-	public void incrementTotalTimeInCPU(int increment) {
-		totalTimeInCPU += increment;
-	} 
+  public int getServiceTime() {
+    return serviceTime;
+  }
 
-	public int timeInCPU() {
-		return totalTimeInCPU;
-	}
+  public String getID() {
+    return ID;
+  }
 
-	public int compareID(Process process) {
-		if(Integer.parseInt(ID.substring(1)) < Integer.parseInt(process.getID().substring(1))) {
-			return -1;
-		}
-		else if(Integer.parseInt(ID.substring(1)) < Integer.parseInt(process.getID().substring(1))) {
-			return 1;
-		}
-		return 0;
-	}
+  public void incrementTotalTimeInCPU(int increment) {
+    totalTimeInCPU += increment;
+  } 
+
+  public int timeInCPU() {
+    return totalTimeInCPU;
+  }
+
+  /*
+    @Input process: A Process object.
+
+    Description:  Process ID's are in the form P1, P2, ... etc. This method compares them 
+                  based on their numerical value. Returns -1 if this process comes before
+                  the process given as parameter, 1 if the other way around and 0 if the
+                  processes have the same ID (which should not happen unless the same 
+                  reference).
+  */
+  public int compareID(Process process) {
+    if(Integer.parseInt(ID.substring(1)) < Integer.parseInt(process.getID().substring(1))) {
+      return -1;
+    }
+    else if(Integer.parseInt(ID.substring(1)) < Integer.parseInt(process.getID().substring(1))) {
+      return 1;
+    }
+    return 0;
+  }
+
+  public boolean completed() {
+    return serviceTime - totalTimeInCPU <= 0;		// Completed when equality.
+  }
 
 }
