5f7592005347879ae1c30af7606647ddee3b18f0
Nathan <nathanbanstess@gmail.com>
2017-08-31 13:11:53 +1000

    Documented.

--- FirstComeFirstServed.java
+++ FirstComeFirstServed.java
@@ -1,21 +1,37 @@
+/*
+  FirstComeFirstServed.java
+
+  Author: Nathan Anstess
+  Student Number: c3202195
+  Date created: 24/08/17
+  Last updated: 29/05/17
+
+  Description: 	An implementation of a Scheduling Algorithm which represents a very basic first come 
+                first served dispatching algorithm.
+*/
+
 import java.util.ArrayList;
 
 public class FirstComeFirstServed implements SchedulingAlgorithm {
 
-	FirstComeFirstServed() {}
+  FirstComeFirstServed() {}
 
-	public void orderProcesses(ArrayList<Process> processes) {}
+  /*
+    Description: FCFS requires no ordering, it is just a basic in/out queue.
+  */
+  public void orderProcesses(ArrayList<Process> processes) {}
 
-	public boolean preemptive() {
-		return false;
-	}
+  public boolean preemptive() {
+    return false;
+  }
 
-	public int getTimeQuantum(Process process) {
-		return process.getServiceTime();
-	}
+  public int getTimeQuantum(Process process) {
+    // Max time allowed in the CPU is it's service time.
+    return process.getServiceTime();
+  }
 
-	public String getAlgorithmName() {
-		return "FCFS";
-	}
+  public String getAlgorithmName() {
+    return "FCFS";
+  }
 
 }
